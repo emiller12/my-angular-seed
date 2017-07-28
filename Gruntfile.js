c203f101b1c98d4466e0c405df20de222eae2260
Elliott Miller <elliottbmiller@gmail.com>
2017-07-28 17:01:10 +0100

    Add grunt watch tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 			build: ['build']
 		},
 		copy: {
-			main: {
+			all: {
 				files:[
 					{
 						expand: true,
@@ -21,20 +21,64 @@ module.exports = function(grunt) {
 					}
 				]
 
+			},
+			one: {
+				files: []
 			}
 		},
 		ts: {
 			src: {
 				tsconfig: './build/src/tsconfig.json',
 				src: ["build/src/**/*.ts"]
+			},
+			one: {
+				tsconfig: './build/src/tsconfig.json',
+				src: []
+			}
+		},
+		watch: {
+			ts: {
+				files: ['src/**/*.ts'],
+				tasks: ['copy:one', 'ts:one'],
+				options: {
+					spawn: false
+				}
+			},
+			templates: {
+				files: ['src/**/*.css', 'src/**/*.html'],
+				tasks: ['copy:one'],
+				options: {
+					spawn: false
+				}
 			}
 		}
 	});
 
+	// On watch events set config for copy:one and ts:one to only run for the changed file
+	grunt.event.on('watch', function(action, filepath, target) {
+		grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+		var newCopyConfig = [
+			{
+				expand: true,
+				src: filepath,
+				dest: 'build/',
+				flatten: false,
+				filter: 'isFile'
+			}
+		];
+		grunt.config('copy.one.files', newCopyConfig);
+
+		if (filepath.slice(-2) == 'ts') {
+			grunt.config('ts.one.src', 'build/' + filepath);
+		}
+
+	});
+
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks("grunt-ts");
 
-	grunt.registerTask("build", ["clean:build", "copy:main", "ts:src"]);
+	grunt.registerTask("build", ["clean:build", "copy:all", "ts:src"]);
 	grunt.registerTask("default", ["build"]);
 };
