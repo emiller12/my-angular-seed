7ce179234e5444ad0fedb03cbfdaa6e45523db7d
Elliott Miller <elliottbmiller@gmail.com>
2017-08-01 11:13:32 +0100

    Install the lodash typings and add a usage example.

--- src/app/components/some-content.component.ts
+++ src/app/components/some-content.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import * as _ from 'lodash';
+import {Component, OnInit} from '@angular/core';
 import {Animal} from '../interfaces/animal';
 
 @Component({
 		selector: 'some-content',
 		templateUrl: 'app/components/some-content.html',
 })
-export class SomeContentComponent {
+export class SomeContentComponent implements OnInit {
 	public animals: Animal[] = [
 		{ name: 'Billy', type: 'Dog' },
 		{ name: 'Blaze', type: 'Pig' },
 		{ name: 'Clucky', type: 'Chicken' }
 	];
+	public animalOfTheDay: Animal;
+
+	ngOnInit() {
+		let randomIndex: number = _.random(0, this.animals.length);
+		this.animalOfTheDay = this.animals[randomIndex];
+	}
 
 }
