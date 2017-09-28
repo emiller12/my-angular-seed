d625f936c58cab41472365d484dab73efc183ccf
Elliott Miller <elliottbmiller@gmail.com>
2017-09-28 14:11:49 +1000

    Add an injectable service for accessing the window object.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } 		from '@angular/forms';
 
+import {WindowRefService} from './services/window-ref.service';
+
 import { AppComponent }  from './components/app.component';
 import { SomeContentComponent }  from './components/some-content.component';
 
 @NgModule({
-	imports:      [ BrowserModule, FormsModule ],
+	imports: [ BrowserModule, FormsModule ],
+	providers: [ WindowRefService ],
 	declarations: [ AppComponent, SomeContentComponent ],
-	bootstrap:    [ AppComponent ]
+	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
